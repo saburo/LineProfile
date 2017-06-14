Fix bug regarding wrong Y-values of moving average
Y-axis of all moving average plots were assigned left side (main)
Y-axis and looked like those have wrong Y-values.

tools/plottingTool.py
@@ -24,7 +24,7 @@ class PlottingTool:
         bgColor = u'#F9F9F9'
         bgColor = u'#E4E4E4'
         spp = mpl.figure.SubplotParams(left=0, bottom=0,
-                                       right=1, top=1, 
+                                       right=1, top=1,
                                        wspace=0, hspace=0)
         self.fig = Figure(figsize=(1, 1),
                           tight_layout=True,
@@ -52,7 +52,7 @@ class PlottingTool:
     #                      direction="in", length=5, width=1, bottom=True,
     #                      top=False, left=True, right=False)
 
-        
+
     #     # for X axis
     #     # major tick
     #     axe1.tick_params(axis="x", which="major", colors=u'k',
@@ -96,7 +96,7 @@ class PlottingTool:
         maY = np.convolve(data[1], np.ones((N,))/N, mode='valid')
         maX = data[0][n2:len(data[0]) - n2 + offset]
         return (maX, maY)
-    
+
     def movingAverage(self, host, data, color, N=10, linestyle='-'):
         maX, maY = self.calculateMovingAverage(data, N)
         movAve, = host.plot(maX, maY, color=color, linestyle=linestyle)
@@ -124,6 +124,7 @@ class PlottingTool:
             for pIndex in xrange(len(pLines)):
                 normFactor.append(1)
 
+        # find index of longest profile line
         longestN = 0
         longestIndex = 0
         for d in xrange(len(data)):
@@ -131,54 +132,15 @@ class PlottingTool:
                 longestN = len(data[d])
                 longestIndex = d
 
+        hostPlotFlag = True
         for d in data[longestIndex]:
             dataN += 1
-            # print map(data, lambda x: len(x['data']))
-            if d['layer_type'] and d['configs']['movingAverage']:
-                for pIndex in xrange(len(data)):
-                    if not len(data[pIndex]):
-                        continue
-                    dd = data[pIndex][dataN-1]
-                    tmp = map(lambda x: x * normFactor[pIndex], dd['data'][0])
-                    dd['data'][0] = tmp
-                    self.movingAverage(self.host, dd['data'], 
-                                       dd['color_org'], 
-                                       dd['configs']['movingAverageN'],
-                                       linestyles[pIndex])
-            # else:
-            #     color = d['color_org']
-
-            if dataN == 1: # host data (1st data)
-                for pIndex in xrange(len(data)):
-                    if not len(data[pIndex]):
-                        continue
-                    dd = data[pIndex][dataN-1]
-                    tmp = map(lambda x: x * normFactor[pIndex], dd['data'][0])
-                    dd['data'][0] = tmp
-                    if d['layer_type'] and d['configs']['movingAverage']:
-                        color = ColorConverter().to_rgba(d['color_org'], alpha=0.1)
-                    else:
-                        color = ColorConverter().to_rgba(d['color_org'], alpha=symbolAlpha[pIndex])
-                    leftA, = self.host.plot(dd['data'][0], dd['data'][1], 
-                                            label=dd['label'], color=color,
-                                            linestyle=linestyles[pIndex],
-                                            linewidth=linewidth[pIndex],
-                                            marker=u'o',
-                                            markersize=self.getMarkerSize(10, len(dd['data'][0])))
-                self.host.set_ylabel(d['label'])
-                self.host.set_xlabel(u"Distance [µm]")
-                self.host.minorticks_on()
-                self.host.axis["bottom"].label.set_fontsize(10)
-                self.host.axis["bottom"].major_ticklabels.set_fontsize(8)
-                self.host.axis["left"].major_ticklabels.set_fontsize(8)
-                self.host.axis["left"].label.set_color(d['color_org'])
-                self.cid = self.mcv.mpl_connect('motion_notify_event', lambda event: self.tracer(event, normFactor))
-                # other connectable event 'button_release_event'
-            else: # after second plot (data)
-            # parasite axes
+            if hostPlotFlag:
+                myAx = self.host
+            else:
                 self.par.append(self.host.twinx())
                 j = len(self.par) - 1
-                # isolated axes
+                # parasite axes
                 if j > 0:
                     offset = 50 * j
                     new_fixed_axis = self.par[j].get_grid_helper().new_fixed_axis
@@ -186,34 +148,61 @@ class PlottingTool:
                                                                axes=self.par[j],
                                                                offset=(offset, 0))
                     self.par[j].axis["right"].toggle(all=True)
-                for pIndex in xrange(len(data)):
-                    if not len(data[pIndex]):
-                        continue
-                    dd = data[pIndex][dataN-1]
-                    tmp = map(lambda x: x * normFactor[pIndex], dd['data'][0])
-                    dd['data'][0] = tmp
-                    if d['layer_type'] and d['configs']['movingAverage']:
-                        color = ColorConverter().to_rgba(d['color_org'], alpha=0.1)
-                    else:
-                        color = ColorConverter().to_rgba(d['color_org'], alpha=symbolAlpha[pIndex])
-                    tmp, = self.par[j].plot(dd['data'][0], dd['data'][1],
-                                            label=dd['label'], color=color,
-                                            linestyle=linestyles[pIndex],
-                                            linewidth=linewidth[pIndex],
-                                            marker=u's',
-                                            markersize=self.getMarkerSize(10, len(dd['data'][0])))
-                self.par[j].set_ylabel(d['label'])
-                self.par[j].minorticks_on()
-                self.par[j].axis["right"].major_ticklabels.set_fontsize(8)
-                self.par[j].axis["right"].label.set_fontsize(10)
-                self.par[j].axis["right"].label.set_color(d['color_org'])
-                # rotate right-labels 180 deg.
-                self.par[j].axis["right"].label.set_axis_direction('left')
-                myRange = self.par[j].axis()
-                myMargin = (myRange[3] - myRange[2]) * AxisPadding
-                self.par[j].set_ylim(myRange[2]-myMargin, myRange[3]+myMargin)
+                myAx = self.par[j]
 
-        # draw vertical line for 
+            for pIndex in xrange(len(data)): # loop for multiple profile lines
+                if not len(data[pIndex]):
+                    continue
+                dd = data[pIndex][dataN-1]
+                tmp = map(lambda x: x * normFactor[pIndex], dd['data'][0])
+                dd['data'][0] = tmp
+
+                if d['layer_type'] and d['configs']['movingAverage']:
+                    self.movingAverage(myAx, dd['data'], dd['color_org'],
+                                       dd['configs']['movingAverageN'],
+                                       linestyles[pIndex])
+
+                alpha = 0.1 if d['layer_type'] and d['configs']['movingAverage'] else symbolAlpha[pIndex]
+                color = ColorConverter().to_rgba(d['color_org'], alpha=alpha)
+                leftA, = myAx.plot(dd['data'][0], dd['data'][1],
+                                   label=dd['label'], color=color,
+                                    linestyle=linestyles[pIndex],
+                                    linewidth=linewidth[pIndex],
+                                    marker=u'o',
+                                    markersize=self.getMarkerSize(10, len(dd['data'][0])))
+            #
+            # Axes styling
+            #
+
+            # common setting
+            myAx.set_ylabel(d['label'])
+            myAx.minorticks_on()
+
+            if hostPlotFlag:
+                # X axis
+                myAx.set_xlabel(u"Distance [µm]")
+                myAx.axis["bottom"].label.set_fontsize(10)
+                myAx.axis["bottom"].major_ticklabels.set_fontsize(8)
+
+                # host Y axis (left side)
+                myAx.axis["left"].major_ticklabels.set_fontsize(8)
+                myAx.axis["left"].label.set_color(d['color_org'])
+
+                # add event listener for marker
+                self.cid = self.mcv.mpl_connect('motion_notify_event', lambda event: self.tracer(event, normFactor))
+            else: # parasite axis (right side)
+                myAx.axis["right"].major_ticklabels.set_fontsize(8)
+                myAx.axis["right"].label.set_fontsize(10)
+                myAx.axis["right"].label.set_color(d['color_org'])
+                # rotate right-labels 180 deg.
+                myAx.axis["right"].label.set_axis_direction('left')
+                myRange = myAx.axis()
+                myMargin = (myRange[3] - myRange[2]) * AxisPadding
+                myAx.set_ylim(myRange[2]-myMargin, myRange[3]+myMargin)
+
+            hostPlotFlag = False
+
+        # draw vertical line for vertices of profile line(s)
         plColor = [u'red', u'blue']
         for pIndex in xrange(len(pLines)):
             d = 0
@@ -245,13 +234,13 @@ class PlottingTool:
             dataN += 1
             if d['layer_type'] and d['configs']['movingAverage']:
                 color = ColorConverter().to_rgba(d['color_org'], alpha=0.1)
-                self.movingAverage(self.host, d['data'], 
+                self.movingAverage(self.host, d['data'],
                                    d['color_org'], d['configs']['movingAverageN'])
             else:
                 color = d['color_org']
 
             if dataN == 1: # host data (1st data)
-                leftA, = self.host.plot(d['data'][0], d['data'][1], 
+                leftA, = self.host.plot(d['data'][0], d['data'][1],
                                         label=d['label'], c=color,
                                         marker=u'o',
                                         linewith=linewidth[pIndex],
@@ -288,7 +277,7 @@ class PlottingTool:
                 self.par[j].axis["right"].label.set_fontsize(10)
                 self.par[j].axis["right"].label.set_color(d['color_org'])
 
-        # draw vertical line for 
+        # draw vertical line for
         d = 0
         for i in range(0, len(pLines) - 1):
             d += pLines[i]['d']